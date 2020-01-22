c31a5fef4d0ff52819eaada085f79748b302db20
Fábio Lira <fabiolira1910@hotmail.com>
2020-01-22 15:17:57 -0300

    home route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express(); // instanciates a express function(which is a built-in cl
 
 const port = 25565;
 
+app.get("/", function(req, res){ //home route
+    res.send("Hello World!")
+});
+
 app.listen(port, function() {
   console.log("Server online on port " + port);
 }); // must be the last command
