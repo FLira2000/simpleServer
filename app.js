221cf1a063f896540f145ba62ab2a1594ef35b7b
Fábio Lira <fabiolira1910@hotmail.com>
2020-01-24 10:49:56 -0300

    adding route

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express(); // instanciates a express function(which is a built-in class) for using
 
-const port = 25565;
+const PORT = 25565;
 
 app.get("/", function(req, res){ //home route
     res.send("<h1> Hello World! </h1>") //including HTML inside 
 });
 
-app.listen(port, function() {
-  console.log("Server online on port " + port);
+app.get("/about", function(req, res){ //about route
+  res.send("<p> Simple Server project created by Fabio L. </p>" + "<p> Any suggestions, send an email.</p>");
+})
+
+app.listen(PORT, function() {
+  console.log("Server online on port " + PORT);
 }); // must be the last command
